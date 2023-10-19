bdbe690b6b4a0515d6476e6c29a1b1bf6b34b22d
Yoseph Zemede <josephzed35@gmail.com>
2023-10-19 23:27:41 +0300

    Done the password generator project

---
+++ password-generator/index.js
@@ -0,0 +1,28 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passwordOneEl = document.getElementById('password-one-el')
+let passwordTwoEl = document.getElementById('password-two-el')
+
+
+function getRandomIndex(){
+    return Math.floor(Math.random() * characters.length)
+}
+
+function generatePassword(){
+    let passwordOne = ""
+    let passwordTwo = ""
+    
+    for (let i = 0; i < 15; i++){
+        console.log(characters[getRandomIndex()])
+        passwordOne += characters[getRandomIndex()]
+        // console.log(passwordOne)
+    }
+
+    for (let i = 0; i < 15; i++){
+        passwordTwo += characters[getRandomIndex()]
+    }
+
+    passwordOneEl.textContent = passwordOne
+    passwordTwoEl.textContent = passwordTwo
+}
